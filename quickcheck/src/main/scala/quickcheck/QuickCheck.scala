255ca31a4c10f88c38c6e303fa8e7f364f5fbd3f
Gerd Reiss <gerdreiss@jscriptive.com>
2016-10-16 13:01:04 +0200

    implemented genHeap, added a couple o'properties

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -1,20 +1,32 @@
 package quickcheck
 
-import common._
-
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Gen._
+import org.scalacheck.Prop._
 import org.scalacheck._
-import Arbitrary._
-import Gen._
-import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] = for {
+    k <- arbitrary[Int]
+    h <- oneOf(const(empty), genHeap)
+  } yield insert(k, h)
+
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
   property("gen1") = forAll { (h: H) =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
+  }
+
+  property("gen2") = forAll { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    val ltm = m - 1
+    findMin(insert(ltm, insert(m, h))) == ltm
+  }
+
+  property("gen3") = forAll { (h: H) =>
+    deleteMin(insert(0, empty)) == empty
   }
 
 }
