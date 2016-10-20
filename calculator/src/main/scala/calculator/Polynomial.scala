b98657fff397c30eb1c200312062d22229b8885b
Gerd Reiss <gerdreiss@jscriptive.com>
2016-10-20 18:21:14 +0200

    Polynomials.scala fixed, Calculator.scala implemented

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,14 +3,15 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
                    c: Signal[Double]): Signal[Double] = {
-    Signal(Math.pow(b(), 2) - 4 * a() * c())
+    Signal(scala.math.pow(b(), 2) - 4 * a() * c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
                        c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    Signal(Set(
-      (-b() + Math.sqrt(delta())) / 2 * a(),
-      (-b() - Math.sqrt(delta())) / 2 * a()
-    ))
+    Signal(delta() match {
+      case d if d < 0 => Set()
+      case d if d == 0 => Set(-b() / (2 * a()))
+      case d => Set((-b() + scala.math.sqrt(delta())) / (2 * a()), (-b() - scala.math.sqrt(delta())) / (2 * a()))
+    })
   }
 }
