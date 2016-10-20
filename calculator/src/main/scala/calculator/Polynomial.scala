5b4fd28891dc052b4b6b2bdf63e54f840f14908f
Gerd Reiss <gerdreiss@jscriptive.com>
2016-10-20 18:29:20 +0200

    third draft for submit

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -12,8 +12,8 @@ object Polynomial {
                        c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
     Signal(delta() match {
       case d if d < 0 => Set()
-      case d if d == 0 => Set(-b() / (2 * a()))
-      case d => Set((-b() + sqrt(delta())) / (2 * a()), (-b() - sqrt(delta())) / (2 * a()))
+      case d if d == 0 => Set(-b() / 2 * a())
+      case d => Set((-b() + sqrt(delta())) / 2 * a(), (-b() - sqrt(delta())) / 2 * a())
     })
   }
 }
